7497e3031a4fab4c17b7c305cad159ec8ba10c75
Ema028 <156372524+Ema028@users.noreply.github.com>
2025-03-27 14:27:08 -0300

    Create list.c

---
+++ list.c
@@ -0,0 +1,37 @@
+# include <stdio.h>
+# include <stdio.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}node;
+
+int get_int(void);
+
+int main(void)
+{
+  node* list = NULL;
+  printf("Quantos números vai ter a lista?\n");
+  int list_size = get_int();
+  for(int i = 0; i < list_size; i ++)
+  {
+    node* n = malloc(sizeof(node));
+    if(n == NULL)
+    {
+      return 1;
+    }
+    printf("Número:\n");
+    n -> number = get_int();
+    n -> next = list;
+    list = n;
+    }
+    return 0;
+}
+
+int get_int(void)
+{
+    int number;
+    scanf("%i\n", &number);
+    return number;
+}
